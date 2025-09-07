89af8e11fb4413a80d20869319ac1c250071a7dc
Sameer Bhardwaj <99178485+Sameer21@users.noreply.github.com>
2025-09-07 21:39:45 +0530

    Create leetcode_1304.java

---
+++ leetcode_1304.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = ((n % 2 == 1) && i == n / 2) ? 0 : (i < n / 2 ? i + 1 : -(n - i));
+        }
+        return a;
+    }
+}
